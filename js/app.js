3e7cd59e152b70ed79e666fd7a1f7f8cf9a39114
Ben Manley <benjamintmanley@gmail.com>
2022-12-13 15:59:55 -0600

    initialize HTML and stub up cached element references

--- js/app.js
+++ js/app.js
@@ -1,12 +1,21 @@
 /*------------ Constants ------------*/
-
+import { getRandomQuote } from "../data/quotes.js"
+import * as taylorAudio from '../js/audio.js'
 
 /*------------ Variables ------------*/
 
 
 /*---- Cached Element References ----*/
 
-/*--------- Event Listeners ---------*/
+const quoteBtn = document.querySelector('#quote-button')
+const cardContainer = document.querySelector('#card-container')
 
+
+
+/*--------- Event Listeners ---------*/
+quoteBtn.addEventListener('click', () => {
+  taylorAudio.playShakeItOff()
+  console.log(getRandomQuote())
+})
 
 /*------------ Functions ------------*/
