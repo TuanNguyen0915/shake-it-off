e5c7cc215cc38f455fb7be3fad8f8af5ffb31ef0
Ben Manley <benjamintmanley@gmail.com>
2022-12-12 22:18:19 -0600

    adjust volume

--- data/audio.js
+++ data/audio.js
@@ -4,18 +4,22 @@ let daylight = new Audio('../assets/daylight.mp3')
 let noNiceThings = new Audio('../assets/no-nice-things.mp3')
 
 function playShakeItOff() {
+  shakeItOff.volume = 0.25
   shakeItOff.play()
 }
 
 function playDarkNight() {
+  darkNight.volume = 0.25
   darkNight.play()
 }
 
 function playDaylight() {
+  daylight.volume = 0.25
   daylight.play()
 }
 
 function playNoNiceThings() {
+  noNiceThings.volume = 0.25
   noNiceThings.play()
 }
 
