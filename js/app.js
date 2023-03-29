5e555d939aa448119f49ce4b683f0ca318acc393
Ben Manley <benjamintmanley@gmail.com>
2023-03-29 13:47:56 -0500

    style the cards/buttons

--- js/app.js
+++ js/app.js
@@ -15,24 +15,25 @@ const cardContainer = document.querySelector('#card-container')
 
 /*--------- Event Listeners ---------*/
 
-quoteBtn.addEventListener('click', ()=> {
-  const newQuote = getRandomQuote()
-  quotes.push(newQuote)
-  console.log(quotes)
-})
+quoteBtn.addEventListener('click', createQuote)
 
 /*------------ Functions ------------*/
 
+function createQuote() {
+  const newQuote = getRandomQuote()
+  quotes.push(newQuote)
+  console.log(quotes)
+}
 
 //// - Add an HTML button that will be used to add a quote
-// - Add a container element for the card components to be appended to
-// - Add a cached element reference for the button
-// - Add a cached element reference for the container element in the JS file
-// - Add an event listener to the button
-// - ~~Write and export a function to access our quote data~~
-// - Import the function that will access the quote data
-// - Ensure the function that we have built will work as intended
-// - Tweak event listeners so that the quote data is stored in a variable
+//// - Add a container element for the card components to be appended to
+//// - Add a cached element reference for the button
+//// - Add a cached element reference for the container element in the JS file
+//// - Add an event listener to the button
+//// - ~~Write and export a function to access our quote data~~
+//// - Import the function that will access the quote data
+//// - Ensure the function that we have built will work as intended
+//// - Tweak event listeners so that the quote data is stored in a variable
 // - Create a card so each quote will have a card associated with it
 // - Create a render function
 // - Add a function to handle appending a card to the container element
