465bf3d98bdb3186a29abeb715e6e74ca7ec16a8
Tuan Nguyen <tuan.nat915@gmail.com>
2023-08-30 14:49:23 -0400

    add funtion adjustFavicon

--- js/app.js
+++ js/app.js
@@ -6,32 +6,57 @@ const quotes = [];
 /*---- Cached Element References ----*/
 const quoteBtn = document.querySelector("#quote-button");
 const cardContainer = document.querySelector("#card-container");
+const favicon = document.getElementById("favicon");
 
 /*--------- Event Listeners ---------*/
 quoteBtn.addEventListener("click", createQuote);
+cardContainer.addEventListener("click", deleteQuote);
 
-/*------------ Functions ------------*/
+/*------------ Function ------------*/
 
 function createQuote() {
   const newQuote = getRandomQuote();
   quotes.push(newQuote);
-  console.log(quotes);
   render();
 }
 
 function render() {
   cardContainer.innerHTML = "";
-  quotes.forEach((quote) => {
-    appendQuote(quote);
+  quotes.forEach((quote, idx) => {
+    appendQuote(quote, idx);
   });
 }
 
-function appendQuote(quote) {
+function appendQuote(quote, idx) {
   let quoteCard = document.createElement("div");
-  quoteCard.className = "card";
+  quoteCard.className = `card ${quote.isTaylor ? "taylor" : "not-taylor"}`;
   quoteCard.innerHTML = `<div>
   <p>${quote.text}</p>
   <p>${quote.isTaylor ? "--T-Swift" : "--A-Hater"}</p>
-  </div>`;
+  </div>
+  <footer>
+  <button class='delete-btn' id='delete-btn-${idx}'>X</button>
+  </footer>
+  `;
   cardContainer.appendChild(quoteCard);
+  adjustFavicon(quote.isTaylor);
+}
+
+function deleteQuote(evt) {
+  if (evt.target.className === "delete-btn") {
+    const idx = evt.target.id.replace("delete-btn-", "");
+    taylorAudio.playNoNiceThings();
+    quotes.splice(idx, 1);
+    render();
+  }
+}
+
+function adjustFavicon(isTaylor) {
+  if (isTaylor) {
+    // set favicon to T-Swift
+    favicon.setAttribute("href", "./assets/taylor-favicon.png");
+  } else {
+    // set favicon to tater tot
+    favicon.setAttribute("href", "./assets/tot.png");
+  }
 }
