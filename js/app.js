86d473c7d8de6cb5c2736225b152a093f3b6a8e7
Tuan Nguyen <tuan.nat915@gmail.com>
2023-08-30 11:56:39 -0400

    Add render(), createQuote(), appendQuote()

--- js/app.js
+++ js/app.js
@@ -1,13 +1,31 @@
 /*------------ Constants ------------*/
 import { getRandomQuote } from "../data/quotes.js";
+import * as taylorAudio from "./audio.js";
 /*------------ Variables ------------*/
-
+const quotes = [];
 /*---- Cached Element References ----*/
 const quoteBtn = document.querySelector("#quote-button");
 const cardContainer = document.querySelector("#card-container");
 
 /*--------- Event Listeners ---------*/
-quoteBtn.addEventListener("click", () => {
-  console.log(getRandomQuote());
-});
+quoteBtn.addEventListener("click", createQuote);
+
 /*------------ Functions ------------*/
+
+function createQuote() {
+  const newQuote = getRandomQuote();
+  quotes.push(newQuote);
+  console.log(quotes);
+  render();
+}
+
+function render() {
+  cardContainer.innerHTML = "";
+  quotes.forEach((quote) => {
+    appendQuote(quote);
+  });
+}
+
+function appendQuote(quote) {
+  console.log("quote is: ", quote);
+}
--- js/audio.js
+++ js/audio.js
@@ -1,31 +1,26 @@
-let shakeItOff = new Audio('../assets/shake-it-off.mp3')
-let darkNight = new Audio('../assets/dark-night.mp3')
-let daylight = new Audio('../assets/daylight.mp3')
-let noNiceThings = new Audio('../assets/no-nice-things.mp3')
+let shakeItOff = new Audio("../assets/shake-it-off.mp3");
+let darkNight = new Audio("../assets/dark-night.mp3");
+let daylight = new Audio("../assets/daylight.mp3");
+let noNiceThings = new Audio("../assets/no-nice-things.mp3");
 
 function playShakeItOff() {
-  shakeItOff.volume = 0.25
-  shakeItOff.play()
+  shakeItOff.volume = 0.25;
+  shakeItOff.play();
 }
 
 function playDarkNight() {
-  darkNight.volume = 0.25
-  darkNight.play()
+  darkNight.volume = 0.25;
+  darkNight.play();
 }
 
 function playDaylight() {
-  daylight.volume = 0.25
-  daylight.play()
+  daylight.volume = 0.25;
+  daylight.play();
 }
 
 function playNoNiceThings() {
-  noNiceThings.volume = 0.25
-  noNiceThings.play()
+  noNiceThings.volume = 0.25;
+  noNiceThings.play();
 }
 
-export {
-  playShakeItOff,
-  playDarkNight,
-  playDaylight,
-  playNoNiceThings
-}
+export { playShakeItOff, playDarkNight, playDaylight, playNoNiceThings };
