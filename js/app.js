8410558a53fcabe5b01c219e9f8e1d90c0922eab
Tuan Nguyen <tuan.nat915@gmail.com>
2023-08-30 10:42:46 -0400

    add quoteBtn and cardContainer element

--- js/app.js
+++ js/app.js
@@ -1,12 +1,11 @@
 /*------------ Constants ------------*/
 
-
 /*------------ Variables ------------*/
 
-
 /*---- Cached Element References ----*/
+const quoteBtn = document.querySelector("#quote-button");
+const cardContainer = document.querySelector("#card-container");
 
 /*--------- Event Listeners ---------*/
-
 
 /*------------ Functions ------------*/
